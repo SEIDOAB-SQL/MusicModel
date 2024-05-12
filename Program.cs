a0a89bbce37a760162ed0a5f42cc907334800976
Martin <martin_lenart@icloud.com>
2024-05-12 20:04:50 +0200

    update

--- Program.cs
+++ Program.cs
@@ -10,21 +10,17 @@ class Program
     }
 
     #region Replaced by new model methods
-    private static void WriteModel(List<csMusicGroup> _greatMusicBands)
+    private static void WriteModel(List<csMusicGroup> _modelList)
     {
-        /*
-        foreach (var band in _greatMusicBands)
-        {
-            Console.WriteLine(band);
-        }
-        */
+        Console.WriteLine($"Nr of great music bands: {_modelList.Count()}");
+        Console.WriteLine($"Total nr of albums produced: {_modelList.Sum(b => b.Albums.Count)}");
+        Console.WriteLine($"Total nr of music band members: {_modelList.Sum(b => b.Members.Count)}");
 
-        Console.WriteLine($"Nr of great music bands: {_greatMusicBands.Count()}");
-        Console.WriteLine($"Total nr of albums produced: {_greatMusicBands.Sum(b => b.Albums.Count)}");
-        Console.WriteLine($"Total nr of music band members: {_greatMusicBands.Sum(b => b.Members.Count)}");
+        Console.WriteLine($"First Music group: {_modelList.First()}");
+        _modelList.First().Albums.ForEach(album => Console.WriteLine($"  - {album.Name}"));
 
-        Console.WriteLine($"First Friend: {_greatMusicBands.First()}");
-        Console.WriteLine($"Last Friend: {_greatMusicBands.Last()}");
+        Console.WriteLine($"Last Music group: {_modelList.Last()}");
+        _modelList.Last().Albums.ForEach(album => Console.WriteLine($"  - {album.Name}"));
 
     }
 
@@ -33,13 +29,25 @@ class Program
         var _seeder = new csSeedGenerator();
 
         //Create a list of 20 great bands
-        var _greatMusicBands = new List<csMusicGroup>();
-        for (int c = 0; c < nrItems; c++)
-        {
-            _greatMusicBands.Add(new csMusicGroup().Seed(_seeder));
-        }
+        var _musicgroups = _seeder.ItemsToList<csMusicGroup>(nrItems);
+        var _artists = _seeder.ItemsToList<csArtist>(nrItems*8);
 
-        return _greatMusicBands;
+        _musicgroups.ForEach(m => {
+
+            //pick 4 to 8 members from the list of _artists
+            m.Members = _seeder.UniqueIndexPickedFromList(_seeder.Next(4, 9), _artists);
+
+            //Create between 5 and 16 Albums
+            m.Albums = new List<csAlbum>();
+            for (int i = 5; i < _seeder.Next(6, 17); i++)
+            {
+                m.Albums.Add(new csAlbum().Seed(_seeder));
+            }
+
+            m.EstablishedYear = m.Albums.Min(a => a.ReleaseYear);
+        });
+
+        return _musicgroups;
     }
     #endregion
 }
--- csMusicGroup.cs
+++ csMusicGroup.cs
@@ -19,26 +19,11 @@ namespace MusicModel
 
         public csMusicGroup Seed(csSeedGenerator _seeder)
         {
-                //Create between 3 and 8 Members
-            var _members = new List<csArtist>();
-            for (int i = 3; i < _seeder.Next(4, 9); i++)
-            {
-                _members.Add(new csArtist().Seed(_seeder));
-            }
-
-            //Create between 5 and 16 Albums
-            var _albums = new List<csAlbum>();
-            for (int i = 5; i < _seeder.Next(6, 17); i++)
-            {
-                _albums.Add(new csAlbum().Seed(_seeder));
-            }
 
             return new csMusicGroup
             {
                 Name = _seeder.MusicGroupName,
-                EstablishedYear = _albums.Min(a => a.ReleaseYear),
-                Members = _members,
-                Albums = _albums,
+                EstablishedYear = 0,
 
                 Seeded = true
             };
